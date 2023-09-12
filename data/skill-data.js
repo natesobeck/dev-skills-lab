9b5d6f1aa1b8082f80a275238b380c78cc8bd0a8
Nate Sobeck <nsobeck910@gmail.com>
2023-09-12 17:37:14 -0400

    complete part 1

---
+++ data/skill-data.js
@@ -0,0 +1,18 @@
+const skills = [
+  { skill: 'pentagon hacking',
+    difficulty: 'easy',
+    hasSkill: true, }, 
+  { skill: 'building killer robots',
+    difficulty: 'easy',
+    hasSkill: true, }, 
+  { skill: 'reading the matrix',
+    difficulty: 'very easy',
+    hasSkill: true, }, 
+  { skill: 'maxing relaxing all cool',
+    difficulty: 'easy',
+    hasSkill: false, }
+]
+
+export {
+  skills
+}
